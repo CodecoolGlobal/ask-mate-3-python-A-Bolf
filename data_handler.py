e8dee75e1e9e5c7a9582bba1aedb9aabd7133d15
A-Bolf <hosc19@gmail.com>
2022-01-10 18:27:51 +0100

    database migration csv->sql almost done TODO:edit_question()

--- data_handler.py
+++ data_handler.py
@@ -7,59 +7,59 @@ DATA_HEADER_QUESTIONS = ['id', 'submission_time', 'view_number', 'vote_number', 
 DATA_HEADER_ANSWERS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
-def get_ordered_questions(order_by, direction):
-    all_questions = connection.get_all_entries(connection.DATA_PATH_QUESTIONS)
-    for one_question in all_questions:
-        for key in one_question:
-            if key in ['id', 'view_number', 'vote_number']:
-                one_question[key] = int(one_question[key])
-    rev = False
-    if direction == 'desc':
-        rev = True
-    sorted_questions_list = sorted(all_questions, key=lambda x: x[order_by], reverse=rev)
-    return sorted_questions_list
+# def get_ordered_questions(order_by, direction):
+#     all_questions = connection.get_all_entries(connection.DATA_PATH_QUESTIONS)
+#     for one_question in all_questions:
+#         for key in one_question:
+#             if key in ['id', 'view_number', 'vote_number']:
+#                 one_question[key] = int(one_question[key])
+#     reverse = False
+#     if direction == 'desc':
+#         reverse = True
+#     sorted_questions_list = sorted(all_questions, key=lambda x: x[order_by], reverse=reverse)
+#     return sorted_questions_list
 
-def write_question(user_question):
-    questions = connection.get_all_entries(connection.DATA_PATH_QUESTIONS)
-    if not questions:
-        identifier = "1"
-    else:
-        all_ids = [int(question['id']) for question in questions]
-        last_id = max(all_ids)
-        identifier = last_id + 1
-    submission_time = int(time.time())
-    question = {
-        'id': identifier,
-        'submission_time': submission_time,
-        DATA_HEADER_QUESTIONS[2]: user_question[2],
-        DATA_HEADER_QUESTIONS[3]: user_question[3],
-        DATA_HEADER_QUESTIONS[4]: user_question[4],
-        DATA_HEADER_QUESTIONS[5]: user_question[5],
-        DATA_HEADER_QUESTIONS[6]: user_question[6]
-    }
-    questions.append(question)
-    connection.write_all_entries(connection.DATA_PATH_QUESTIONS, connection.DATA_HEADER_QUESTIONS, questions)
+# def write_question(user_question):
+#     # questions = connection.get_all_entries(connection.DATA_PATH_QUESTIONS)
+#     # if not questions:
+#     #     identifier = "1"
+#     else:
+#         all_ids = [int(question['id']) for question in questions]
+#         last_id = max(all_ids)
+#         identifier = last_id + 1
+#     submission_time = int(time.time())
+#     question = {
+#         'id': identifier,
+#         'submission_time': submission_time,
+#         DATA_HEADER_QUESTIONS[2]: user_question[2],
+#         DATA_HEADER_QUESTIONS[3]: user_question[3],
+#         DATA_HEADER_QUESTIONS[4]: user_question[4],
+#         DATA_HEADER_QUESTIONS[5]: user_question[5],
+#         DATA_HEADER_QUESTIONS[6]: user_question[6]
+#     }
+#     questions.append(question)
+#     connection.write_all_entries(connection.DATA_PATH_QUESTIONS, connection.DATA_HEADER_QUESTIONS, questions)
 
 
 
-def write_answer(answer,question_id):
-    answers = connection.get_all_entries(connection.DATA_PATH_ANSWERS)
-    if not answers:
-        identifier = "1"
-    else:
-        all_ids = [int(answer['id']) for answer in answers]
-        last_id = max(all_ids)
-        identifier = last_id + 1
-    answer_dict = {
-        DATA_HEADER_ANSWERS[0]: identifier,
-        DATA_HEADER_ANSWERS[1]: int(time.time()),
-        DATA_HEADER_ANSWERS[2]: "0",
-        DATA_HEADER_ANSWERS[3]: question_id,
-        DATA_HEADER_ANSWERS[4]: answer[0],
-        DATA_HEADER_ANSWERS[5]: answer[1]
-    }
-    answers.append(answer_dict)
-    connection.write_all_entries(connection.DATA_PATH_ANSWERS, connection.DATA_HEADER_ANSWERS, answers)
+# def write_answer(answer,question_id):
+#     answers = connection.get_all_entries(connection.DATA_PATH_ANSWERS)
+#     if not answers:
+#         identifier = "1"
+#     else:
+#         all_ids = [int(answer['id']) for answer in answers]
+#         last_id = max(all_ids)
+#         identifier = last_id + 1
+#     answer_dict = {
+#         DATA_HEADER_ANSWERS[0]: identifier,
+#         DATA_HEADER_ANSWERS[1]: int(time.time()),
+#         DATA_HEADER_ANSWERS[2]: "0",
+#         DATA_HEADER_ANSWERS[3]: question_id,
+#         DATA_HEADER_ANSWERS[4]: answer[0],
+#         DATA_HEADER_ANSWERS[5]: answer[1]
+#     }
+#     answers.append(answer_dict)
+#     connection.write_all_entries(connection.DATA_PATH_ANSWERS, connection.DATA_HEADER_ANSWERS, answers)
 
 
 def delete_question(question_id):
--- server.py
+++ server.py
@@ -4,13 +4,14 @@ import os
 import time
 import connection
 import data_handler
+import data_manager
 
 UPLOAD_FOLDER = './static'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ORDER_BY = 'submission_time'
-ORDER_DIRECTION = 'asc'
+ORDER_BY = "submission_time"
+ORDER_DIRECTION = "asc"
 
 
 @app.route("/static/<path:path>")
@@ -25,7 +26,7 @@ def main_page():
 
 @app.route('/list')
 def route_list():
-    user_questions = data_handler.get_ordered_questions(ORDER_BY, ORDER_DIRECTION)
+    user_questions = data_manager.get_ordered_questions(ORDER_BY, ORDER_DIRECTION)
     return render_template('list.html', user_questions=user_questions, header=data_handler.DATA_HEADER_QUESTIONS)
 
 
@@ -34,13 +35,13 @@ def route_list_order(order_by):
     global ORDER_BY
     global ORDER_DIRECTION
     if order_by == ORDER_BY:
-        if ORDER_DIRECTION == 'asc':
-            ORDER_DIRECTION = 'desc'
+        if ORDER_DIRECTION == "asc":
+            ORDER_DIRECTION = "desc"
         else:
-            ORDER_DIRECTION = 'asc'
+            ORDER_DIRECTION = "asc"
     else:
         ORDER_BY = order_by
-    user_questions = data_handler.get_ordered_questions(ORDER_BY, ORDER_DIRECTION)
+    user_questions = data_manager.get_ordered_questions(ORDER_BY, ORDER_DIRECTION)
     return render_template('list.html', user_questions=user_questions, header=data_handler.DATA_HEADER_QUESTIONS)
 
 
@@ -53,100 +54,101 @@ def add_question():
             if uploaded_file.filename != '':
                 uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename)))
                 image = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename))
-        submission_time = int(time.time())
-        view_number = "0"
-        vote_number = "0"
         title = request.form.get("title")
         message = request.form.get("message")
-        data_handler.write_question(['x', 'x', view_number, vote_number, title, message, image])
+        data_manager.write_question(title=title, message=message, image=image)
         return redirect(url_for("route_list"))
     return render_template("add_question.html", headers=data_handler.DATA_HEADER_QUESTIONS)
 
 
 @app.route('/question/<question_id>')
 def question_page(question_id):
-    answers = connection.get_all_entries(connection.DATA_PATH_ANSWERS)
-    user_questions = connection.get_all_entries(connection.DATA_PATH_QUESTIONS)
-    for row in user_questions:
-        if row["id"] == question_id:
-            row["view_number"] = int(row.get('view_number', 0)) + 1
-    connection.write_all_entries(connection.DATA_PATH_QUESTIONS, connection.DATA_HEADER_QUESTIONS, user_questions)
-    one_question = {}
-    for question in user_questions:
-        if question["id"] == question_id:
-            one_question = question
+    answers = data_manager.get_answers()
+    data_manager.increase_view_count(table='question',id=question_id)
+    one_question=data_manager.get_question_by_id(id=question_id)
+    # user_questions = data_manager.get_questions()
+    # for row in user_questions:
+    #     if row["id"] == question_id:
+    #         row["view_number"] = int(row.get('view_number', 0)) + 1
+    # connection.write_all_entries(connection.DATA_PATH_QUESTIONS, connection.DATA_HEADER_QUESTIONS, user_questions)
+    # one_question = {}
+    # for question in user_questions:
+    #     if question["id"] == question_id:
+    #         one_question = question
     return render_template('one_question.html', question_id=question_id, one_question=one_question, answers=answers)
 
 
 @app.route('/question/<question_id>/new-answer', methods=["POST", "GET"])
 def new_answer(question_id):
     if request.method == "POST":
-        message = [request.form.get("message"), ""]
+        message = request.form.get("message")
+        image = request.form.get("image")
         if request.files["file"]:
             uploaded_file = request.files['file']
             if uploaded_file.filename != '':
                 uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename)))
-                message = [request.form.get("message"),
-                           os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename))]
-        data_handler.write_answer(message, question_id)
+                image = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(uploaded_file.filename))
+        data_manager.write_answer(question_id=question_id,message=message,image=image)
         return redirect(url_for("question_page", question_id=question_id))
     return render_template('answer.html', question_id=question_id)
 
 
 @app.route('/question/<question_id>/delete')
 def question_page_delete(question_id):
-    data_handler.delete_question(question_id)
+    data_manager.delete_question_by_id(id=question_id)
     return redirect('/list')
 
 
 @app.route('/question/<question_id>/vote/<up_or_down>')
 def question_page_vote(question_id, up_or_down):
-    all_questions = connection.get_all_entries(connection.DATA_PATH_QUESTIONS)
-    for row in all_questions:
-        if row["id"] == question_id:
-            if up_or_down == 'up':
-                row["vote_number"] = int(row.get('vote_number', 0)) + 1
-            elif up_or_down == 'down':
-                row["vote_number"] = int(row.get('vote_number', 0)) - 1
-    connection.write_all_entries(connection.DATA_PATH_QUESTIONS, connection.DATA_HEADER_QUESTIONS, all_questions)
+    # all_questions = data_manager.get_questions()
+    # for row in all_questions:
+    #     if row["id"] == question_id:
+    #         if up_or_down == 'up':
+    #             row["vote_number"] = int(row.get('vote_number', 0)) + 1
+    #         elif up_or_down == 'down':
+    #             row["vote_number"] = int(row.get('vote_number', 0)) - 1
+    # connection.write_all_entries(connection.DATA_PATH_QUESTIONS, connection.DATA_HEADER_QUESTIONS, all_questions)
+    data_manager.set_vote_count(table='question',id=question_id,up_or_down=up_or_down)
     return redirect('/list')
 
 
-@app.route('/question/<question_id>/edit', methods= ["POST","GET"])
+@app.route('/question/<question_id>/edit', methods=["POST", "GET"])
 def edit_question(question_id):
-    all_questions = connection.get_all_entries(connection.DATA_PATH_QUESTIONS)
+    all_questions = data_manager.get_questions()
     for row in all_questions:
         if row["id"] == question_id:
-            user_question=row
+            user_question = row
     if request.method == 'POST':
         for row in all_questions:
             if row["id"] == question_id:
                 row["submission_time"] = int(time.time())
                 row["title"] = request.form.get("title")
                 row["message"] = request.form.get("message")
-        connection.write_all_entries(connection.DATA_PATH_QUESTIONS,connection.DATA_HEADER_QUESTIONS,all_questions)
+        connection.write_all_entries(connection.DATA_PATH_QUESTIONS, connection.DATA_HEADER_QUESTIONS, all_questions)
         return redirect('/')
     return render_template('edit_question.html', question_id=question_id, user_question=user_question)
 
 
 @app.route('/answer/<answer_id>/delete')
 def answer_delete(answer_id):
-    question_id = data_handler.get_answer_question_id(answer_id)
-    data_handler.delete_answer(answer_id)
+    question_id = data_manager.get_question_id_by_answer_id(answer_id=answer_id)['question_id']
+    data_manager.delete_answer_by_id(id=answer_id)
     return redirect('/question/' + question_id)
 
 
 @app.route('/answer/<answer_id>/vote/<up_or_down>')
 def answer_page_vote(answer_id, up_or_down):
-    all_answers = connection.get_all_entries(connection.DATA_PATH_ANSWERS)
-    for row in all_answers:
-        if row["id"] == answer_id:
-            if up_or_down == 'up':
-                row["vote_number"] = int(row.get('vote_number', 0)) + 1
-            elif up_or_down == 'down':
-                row["vote_number"] = int(row.get('vote_number', 0)) - 1
-    connection.write_all_entries(connection.DATA_PATH_ANSWERS, connection.DATA_HEADER_ANSWERS, all_answers)
-    question_id = data_handler.get_answer_question_id(answer_id)
+    # all_answers = data_manager.get_answers()
+    # for row in all_answers:
+    #     if row["id"] == answer_id:
+    #         if up_or_down == 'up':
+    #             row["vote_number"] = int(row.get('vote_number', 0)) + 1
+    #         elif up_or_down == 'down':
+    #             row["vote_number"] = int(row.get('vote_number', 0)) - 1
+    # connection.write_all_entries(connection.DATA_PATH_ANSWERS, connection.DATA_HEADER_ANSWERS, all_answers)
+    data_manager.set_vote_count(table='answer',id=answer_id,up_or_down=up_or_down)
+    question_id = data_manager.get_question_id_by_answer_id(answer_id=answer_id)
     return redirect('/question/' + question_id)
 
 
