685c39673fad985974f7ff831f427eb374f8a9c2
A-Bolf <hosc19@gmail.com>
2022-01-27 13:20:40 +0100

    add search functionality to bonus questions

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -1,13 +1,7 @@
 // you receive an array of objects which you must sort in the by the key "sortField" in the "sortDirection"
+var size=15;
+document.getElementById('table').style.fontSize=size.toString()+'px';
 function getSortedItems(items, sortField, sortDirection) {
-    console.log(items)
-    console.log(sortField)
-    console.log(sortDirection)
-
-    // === SAMPLE CODE ===
-    // if you have not changed the original html uncomment the code below to have an idea of the
-    // effect this function has on the table
-    //
     if (sortDirection === "asc") {
         const firstItem = items.shift()
         if (firstItem) {
@@ -23,24 +17,31 @@ function getSortedItems(items, sortField, sortDirection) {
     return items
 }
 
-// you receive an array of objects which you must filter by all it's keys to have a value matching "filterValue"
 function getFilteredItems(items, filterValue) {
-    console.log(items)
-    console.log(filterValue)
+    let results = [];
+    for (let i=0; i<items.length; i++) {
+        if (filterValue[0] ==='!'){
+            if (filterValue.includes('Description')){
+                if (!items[i]['Description'].includes(filterValue.substr(13,filterValue.length))){
+                    results.push(items[i])
+                }
+            }
+            else if (!items[i]['Title'].includes(filterValue.substr(1,filterValue.length))){
+                results.push(items[i])
+            }
+        }
+        else if (filterValue.includes('Description')){
+            if (items[i]['Description'].includes(filterValue.substr(12,filterValue.length))){
+                results.push(items[i])
+            }
 
-    // === SAMPLE CODE ===
-    // if you have not changed the original html uncomment the code below to have an idea of the
-    // effect this function has on the table
-    //
-    // for (let i=0; i<filterValue.length; i++) {
-    //     items.pop()
-    // }
-    function checkForValue(element) {
-        return filterValue in element;
-}
+        }
+        else if (items[i]['Title'].includes(filterValue)){
+            results.push(items[i])
+        }
 
-    filtered_items = items.filter(checkForValue)
-    return filtered_items
+    }
+    return results
 }
 
 
@@ -51,29 +52,18 @@ function toggleTheme() {
    element.classList.toggle("dark-mode")
 }
 
-var min = 10;
-var max = 25;
-
-function changeFontSize(delta) {
-  var tags = document.querySelectorAll('input, table, button');
-  for (i = 0; i < tags.length; i++) {
-    if (tags[i].style.fontSize) {
-      var s = parseInt(tags[i].style.fontSize.replace("px", ""));
-    } else {
-      var s = 17;
-    } if (s != max) {
-      s += delta;
-    }
-    tags[i].style.fontSize = s + "px"
-  }
-}
-
 function increaseFont() {
-    changeFontSize(1);
-    console.log("increaseFont");
+    if (size<=20) {
+        size++
+    }
+
+    document.getElementById('table').style.fontSize=size.toString()+'px'
 }
 
 function decreaseFont() {
-    console.log("decreaseFont");
-    changeFontSize((-1));
+    if (size>=3) {
+        size--
+    }
+
+    document.getElementById('table').style.fontSize=size.toString()+'px'
 }
